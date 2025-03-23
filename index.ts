dca5fcfdd79f3fbc9e45e94a276be37c404428c1
andjsrk <andjsrk0213@gmail.com>
2025-03-23 19:34:46 +0900

    add special operations

--- index.ts
+++ index.ts
@@ -1,4 +1,13 @@
-import { createSignal, type SignalOptions, type Accessor, type Setter, type Signal as SolidSignal } from 'solid-js'
+import type { SignalOptions, Accessor, Setter, Signal as SolidSignal } from 'solid-js'
+import { createSignal, equalFn as solidDefaultEquals } from 'solid-js'
+
+/**
+ * Indicates a signal that is wrapped using {@linkcode wrap}.
+ * Some features are lacking in contrast to {@linkcode Signal}.
+ */
+export interface WrappedSignal<T> extends Accessor<T> {
+	set: Setter<T>
+}
 
 /**
  * A more concise and less error-prone form for signals.
@@ -38,8 +47,12 @@ import { createSignal, type SignalOptions, type Accessor, type Setter, type Sign
  * acceptsSignal(name)
  * ```
  */
-export interface Signal<T> extends Accessor<T> {
-	set: Setter<T>
+export interface Signal<T> extends WrappedSignal<T>, SignalOperations<T> {}
+export interface SignalOperations<T> {
+	/**
+	 * @returns Whether the value is equal to the signal's current value.
+	 */
+	equals: (x: T) => boolean
 }
 
 /**
@@ -50,7 +63,7 @@ export interface Signal<T> extends Accessor<T> {
  * const isOpen = wrap(createSignal(false))
  * ```
  */
-export const wrap = <T>([value, setValue]: SolidSignal<T>): Signal<T> => {
+export const wrap = <T>([value, setValue]: SolidSignal<T>): WrappedSignal<T> => {
 	function get() {
 		return value()
 	}
@@ -60,13 +73,41 @@ export const wrap = <T>([value, setValue]: SolidSignal<T>): Signal<T> => {
 
 /**
  * A shortcut for `wrap(createSignal())`.
+ * 
+ * @see {@linkcode wrap}
+ * @see {@linkcode createSignal}
  */
 export function create<T>(): Signal<T | undefined>
 /**
  * A shortcut for `wrap(createSignal(...))`.
+ * 
+ * @see {@linkcode wrap}
+ * @see {@linkcode createSignal}
  */
 export function create<T>(value: T, options?: SignalOptions<T>): Signal<T>
-export function create<T>(...args: [] | [value: T, optiona?: SignalOptions<T>]): Signal<T | undefined> {
-	return wrap(createSignal<T>(...args as []))
+export function create<T>(...args: [value?: T, options?: SignalOptions<T | undefined>]): Signal<T | undefined> {
+	const wrapped = wrap(createSignal<T | undefined>(...args))
+	
+	const [_, options] = args
+	let equalsFn: SignalOperations<T>['equals']
+	if (options && 'equals' in options) {
+		const { equals } = options
+		if (equals === false) {
+			// NOTE: `equals` in `SignalOptions` and `equals` in `Signal` have different purpose
+			// so the current behavior would be more reasonable
+			
+			// equalsFn = () => false
+			equalsFn = x => solidDefaultEquals(wrapped(), x)
+		} else {
+			equalsFn = x => equals(wrapped()!, x)
+		}
+	} else {
+		equalsFn = x => solidDefaultEquals(wrapped(), x)
+	}
+	const ops: SignalOperations<T> = {
+		equals: equalsFn,
+	}
+	
+	return Object.assign(wrapped, ops as SignalOperations<T | undefined>)
 }
 export { create as signal, create as createSignal }
